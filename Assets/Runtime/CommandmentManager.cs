fba3b6a0b5e1ec361a5539aaa7b089dd0ad3d96c
joshsylvan <9077523+joshsylvan@users.noreply.github.com>
2025-01-25 22:12:35 +0000

    Add Action completion

--- Assets/Runtime/CommandmentManager.cs
+++ Assets/Runtime/CommandmentManager.cs
@@ -1,15 +1,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.Animations;
+
+public class CommandmentData
+{
+    public float reward;
+    public float time;
+    public Activity[] activities;
+    public Dictionary<Activity, int> activityCountMap = new();
+
+    public CommandmentData(Commandment data)
+    {
+        reward = data.reward;
+        time = data.time;
+        activities = data.activities;
+        foreach (var activity in activities)
+        {
+            if (!activityCountMap.ContainsKey(activity))
+            {
+                activityCountMap.Add(activity, 0);
+            }
+            activityCountMap[activity] += 1;
+        }
+    }
+}
+
 
 public class CommandmentManager : MonoBehaviour
 {
     [SerializeField]
     public Commandment[] CommandmentData;
     const int MAX_COMMANDMENTS = 3;
-    private List<Commandment> _commandments = new();
-
+    private List<CommandmentData> _commandments = new();
     public event Action OnNewCommandmentAdded;
+    public event Action OnCommandmentCompleted;
+
+
+
+    private Dictionary<Activity, int> _performedActivityCounts = new();
 
     void Awake()
     {
@@ -28,8 +57,62 @@ public class CommandmentManager : MonoBehaviour
     {
         if (_commandments.Count < MAX_COMMANDMENTS)
         {
-            _commandments.Insert(_commandments.Count, GetNextCommandment());
+            _commandments.Insert(_commandments.Count, new CommandmentData(GetNextCommandment()));
             OnNewCommandmentAdded?.Invoke();
         }
+    }
+
+    public void PerformActivityForFrame(Activity activity)
+    {
+        if (!_performedActivityCounts.ContainsKey(activity))
+        {
+            _performedActivityCounts.Add(activity, 0);
+        }
+        _performedActivityCounts[activity] += 1;
+    }
+
+    bool HasCompletedCommand(CommandmentData commandment)
+    {
+        foreach (var (activity, count) in commandment.activityCountMap)
+        {
+            if (!_performedActivityCounts.ContainsKey(activity))
+            {
+                return false;
+            }
+            if (_performedActivityCounts[activity] < count)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void Update()
+    {
+        for (int i = 0; i < _commandments.Count;)
+        {
+            CommandmentData command = _commandments[i];
+            if (HasCompletedCommand(command))
+            {
+                foreach (var (activity, count) in command.activityCountMap)
+                {
+                    _performedActivityCounts[activity] -= count;
+                }
+
+                command.time -= Time.deltaTime;
+
+                if (command.time <= 0)
+                {
+                    _commandments.RemoveAt(i);
+                    Debug.Log("Task Complete!");
+                    OnCommandmentCompleted?.Invoke();
+
+                    continue;
+                }
+            }
+            i += 1;
+        }
+
+        _performedActivityCounts.Clear();
     }
 }

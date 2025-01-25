f4744c663a616f5ce620267807b6046e964d70a9
Paul Hayes <paul.hayes@entropedia.co.uk>
2025-01-25 13:43:17 +0000

    Culty cultists

--- Assets/Runtime/CultistManager.cs
+++ Assets/Runtime/CultistManager.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class CultistManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Cultist m_cultistPrefab;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         

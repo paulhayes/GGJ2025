467156a213873e02830c2d42d668c5cddbb83d5d
joshsylvan <9077523+joshsylvan@users.noreply.github.com>
2025-01-26 02:19:56 +0000

    Simple Commandment UI

---
+++ Assets/Runtime/ActivityIconData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ActivityIconData", menuName = "Scriptable Objects/ActivityIconData")]
+public class ActivityIconData : ScriptableObject
+{
+    [SerializeField] public Texture BlowingBubblesIcon;
+    [SerializeField] public Texture DancingIcon;
+    [SerializeField] public Texture BubbleTeaIcon;
+    [SerializeField] public Texture WashingUpIcon;
+    [SerializeField] public Texture PrayingIcon;
+    [SerializeField] public Texture PumpingIcon;
+    [SerializeField] public Texture RecruitingIcon;
+    [SerializeField] public Texture RestingIcon; 
+}

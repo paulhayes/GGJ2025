6ed81a39b5d4d8d88474450ce8b6bba567cf8f17
joshsylvan <9077523+joshsylvan@users.noreply.github.com>
2025-01-26 15:15:06 +0000

    Add bubble health to game data

--- Assets/Runtime/GameData.cs
+++ Assets/Runtime/GameData.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 [CreateAssetMenu()]
 public class GameData : ScriptableObject
 {
-    HashSet<UnityEngine.Object> m_players;
-    public float BubbleHealth { get; set; }
+    HashSet<UnityEngine.Object> m_players = new();
+
+    [SerializeField] public float BubbleHealth = 100.0f;
 
     public int NumPlayers()
     {
-        return m_players.Count; 
+        return m_players.Count;
     }
 
     public void RegisterPlayer(UnityEngine.Object player)
